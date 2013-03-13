fc6ad8680dfcce3025532495c71e80beef805007
Henrik Lundqvist <henrik.lundqvist89@gmail.com>
2013-03-13 11:53:27 +0100

    v 1.0.0
    RED - ett MVC-ramverk.

---
+++ incl/byline.php
@@ -0,0 +1,22 @@
+<!-- by-line -->
+
+<footer class = "byline">
+
+	<figure class = "right top" >
+		<img src= "img/minime.jpg"
+			alt = "Liten bild på Henrik"
+			height = "50" 
+			
+			/>	
+	</figure>
+
+	<p>/Henrik
+	</p>
+	
+	<p> Henrik Lundqvist är en distansstudent vid BTH. Tidigare har han studerat
+	speldesign och serious games vid Högskolan i Skövde.
+	</p>
+
+</footer>
+
+
